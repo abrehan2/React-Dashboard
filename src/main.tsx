d3300b8e4fc877b62be6a1174fd99de4e098afc7
abrehan2 <rehannajam2@gmail.com>
2024-01-23 19:18:03 +0500

    BREAKING CHANGE: SETTING UP SASS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 // IMPORTS -
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import "./styles/app.scss";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
