62d16f952b8cbd9b07105b06dda9e3225ffc062a
abrehan2 <rehannajam2@gmail.com>
2024-01-23 20:20:59 +0500

    SETTING UP BASIC STYLES

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 // IMPORTS -
-import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// SCREENS -
+import {
+  Dashboard,
+  Products,
+  Customers,
+  Transaction,
+} from "./utils/lazyRoutes";
+import { Suspense } from "react";
+import Loader from "./components/Loader";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
 
-export default App
+export default App;
+
+
+// NOTES:
+// JAB TAK YE COMPONENTS LOAD NAE HOTE SARAY TAB TAK HUM LOADER DEKHA SAKTE AND USKE LIYE WE CAN KEEP THESE ROUTES IN SUSPENSE 
+// COMPONENTS ARE DYNAMICALLY IMPORTED USING LAZY (CHUNKS MAIN COMPONENTS BHEJTE HAI AND NOT IN BUNDLE, ISAY CODE SPLITTING BHI KHETE HAI. HAR PAGE KA COMPONENT TAB LOAD HO JAB WO TRIGGER HO)
---
+++ src/utils/lazyRoutes.tsx
@@ -0,0 +1,8 @@
+// IMPORTS -
+import { lazy } from "react";
+
+// ROUTES -
+export const Dashboard = lazy(() => import("../pages/Dashboard"));
+export const Products = lazy(() => import("../pages/Products"));
+export const Transaction = lazy(() => import("../pages/Transaction"));
+export const Customers = lazy(() => import("../pages/Customers"));
