b6e1af9efb2b0fb9f8c61b7cdfaaeecdefff425c
abrehan2 <rehannajam2@gmail.com>
2024-01-31 01:57:33 +0500

    BREAKING CHANGE: INTEGRATED PIE AND DOUGNUT CHARTS

--- src/pages/charts/PieChart.tsx
+++ src/pages/charts/PieChart.tsx
@@ -1,5 +1,7 @@
 // IMPORTS -
 import Sidebar from "../../components/Sidebar";
+import {DoughnutChart, PieChart as Pie} from "../../components/Charts";
+import {categories} from "../../assets/data.json";
 
 
 
@@ -10,15 +12,30 @@ const PieChart = () => {
       <main className="chartContainer">
         <h1>Pie chart(s)</h1>
         <section>
-
           <div>
-            
+            <Pie
+              labels={["Processing", "Shipped", "Delivered"]}
+              data={[10, 20, 30]}
+              backgroundColor={["#061826", "#3b71ca", "#f26419"]}
+              offset={[5, 5, 5]}
+            />
           </div>
-          
-          <h2>Top selling products and customers</h2>
+
+          <h2>Order ratio</h2>
         </section>
 
-        
+        <section>
+          <div>
+            <DoughnutChart
+              labels={categories.map((i) => i.heading)}
+              data={categories.map((i) => i.value)}
+              backgroundColor={["#061826", "#3b71ca", "#f26419", "#78C091"]}
+              offset={[5, 5, 5]}
+            />
+          </div>
+
+          <h2>Product categories ratio</h2>
+        </section>
       </main>
     </div>
   );
